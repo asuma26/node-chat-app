09d21d84b224a1176986a7948c8405c72b998517
Ashish Mishra <as06260@gmail.com>
2018-01-24 23:29:09 +0530

    Added autoscroll feature

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket=io();// stroes socket  in a variable and critical for makeing connections
 
+
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+
 socket.on('connect',function(){
   console.log('connected to server');
 
@@ -29,6 +47,7 @@ socket.on('newMessage',(message)=>{
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -43,7 +62,7 @@ socket.on('newLocationMessage', function (message) {
 
    jQuery('#messages').append(html);
 
-
+scrollToBottom();
 
 
 });
